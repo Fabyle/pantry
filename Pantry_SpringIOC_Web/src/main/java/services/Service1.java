c6b3aa328ccba54da5277bf6b20099b2807de64d
fabyle <fabien99@gmail.com>
2013-01-13 18:25:22 +0100

    Signed-off-by: fabyle <fabien99@gmail.com>

---
+++ Pantry_SpringIOC_Web/src/main/java/services/Service1.java
@@ -0,0 +1,7 @@
+package services;
+
+public interface Service1 {
+	
+	public String getName();
+
+}
---
+++ Pantry_SpringIOC_Web/src/main/java/services/imp/Service1Imp.java
@@ -0,0 +1,14 @@
+package services.imp;
+
+import services.Service1;
+
+public class Service1Imp implements Service1 {
+
+	@Override
+	public String getName() {
+		return "I'm service 1";
+	}
+
+	
+
+}
