8e2a567785b74af361d502f341afc8504926f1f7
fabyle <fabien99@gmail.com>
2013-01-24 11:50:42 +0100

    Signed-off-by: fabyle <fabien99@gmail.com>

--- teamManagingCalendar/src/test/java/com/fabyle/team/managing/Services/Test_IPOIExcelServicesImp.java
+++ teamManagingCalendar/src/test/java/com/fabyle/team/managing/Services/Test_IPOIExcelServicesImp.java
@@ -1,20 +1,28 @@
 package com.fabyle.team.managing.Services;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import junit.framework.TestCase;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import com.fabyle.team.managing.Services.imp.POIExcelServicesImp;
 
 public class Test_IPOIExcelServicesImp extends TestCase {
-	
-	public void testTest() {
-		
+
+	public void testWorkbook() {
+
+		SimpleDateFormat formatFile = new SimpleDateFormat(
+				"dd-MM-yyyy-hh-mm-ss");
+
 		POIExcelServicesImp service = new POIExcelServicesImp();
-		service.test();
-		
-		
-		
-		
+
+		HSSFWorkbook wkb = service.initWorkbook();
+		service.createGrilleJours(wkb, "01-01-2013", "31-03-2013");
+		service.SaveWorkbook("planning-" + (formatFile.format(new Date()))
+				+ ".xls", wkb);
+
 	}
-	
 
 }
